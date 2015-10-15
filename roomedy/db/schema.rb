ecc094a8a57ad390ff5d0d413f867e41c4f2213a
dschrag <dschrag@purdue.edu>
2015-10-15 00:03:43 -0400

    Creating notes

--- roomedy/db/schema.rb
+++ roomedy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014160311) do
+ActiveRecord::Schema.define(version: 20151015032931) do
 
   create_table "houses", force: :cascade do |t|
     t.string   "name"
@@ -24,11 +24,14 @@ ActiveRecord::Schema.define(version: 20151014160311) do
   end
 
   create_table "notes", force: :cascade do |t|
-    t.string   "message"
-    t.integer  "createdByUser"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  add_index "notes", ["user_id", "created_at"], name: "index_notes_on_user_id_and_created_at"
+  add_index "notes", ["user_id"], name: "index_notes_on_user_id"
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "house_id"
