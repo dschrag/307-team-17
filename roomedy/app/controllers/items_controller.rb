7bde60321a5b94b960a78ab65cd279a06b8c814f
srypher <bduff245@gmail.com>
2015-10-16 17:18:32 +0000

    Edit/Delete Functionality for Items

--- roomedy/app/controllers/items_controller.rb
+++ roomedy/app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+    before_action :correct_user, only: :destroy
     
     def new
         @item = Item.new
@@ -17,8 +18,34 @@ class ItemsController < ApplicationController
     def index
         @items = Item.paginate(page: params[:page])
     end
+    
+    def edit
+        @item = Item.find(params[:id])
+    end
+
+    def update
+         @item = Item.find(params[:id])
+         if @item.update_attributes(item_params)
+            flash[:success] = "Item successfully updated"
+            redirect_to items_path
+         else
+            render 'edit'
+         end
+    end
+    
+    def destroy
+        @item.destroy
+        flash[:success] = "Item Deleted"
+        redirect_to items_path
+    end
+    
     private
     	def item_params
 			params.require(:item).permit(:item_amount, :item_price, :item_name)
 		end
+		
+		def correct_user
+		    @item = current_user.items.find_by(id: params[:id])
+		    redirect_to root_url if @item.nil?
+	    end
 end
