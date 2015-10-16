541af144bb29492f7510a8ea5184b5e02413bac1
aksaram2716 <agudoor@purdue.edu>
2015-10-15 23:57:37 -0400

    notes and some gui changes some more gui changes to be done

--- roomedy/db/schema.rb
+++ roomedy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015233216) do
+ActiveRecord::Schema.define(version: 20151015032931) do
 
   create_table "houses", force: :cascade do |t|
     t.string   "name"
@@ -57,20 +57,9 @@ ActiveRecord::Schema.define(version: 20151015233216) do
   add_index "relationships", ["house_id"], name: "index_relationships_on_house_id"
   add_index "relationships", ["user_id"], name: "index_relationships_on_user_id"
 
-  create_table "share_notes", force: :cascade do |t|
-    t.integer  "note_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "share_notes", ["note_id"], name: "index_share_notes_on_note_id"
-  add_index "share_notes", ["user_id"], name: "index_share_notes_on_user_id"
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.integer  "houseID"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
