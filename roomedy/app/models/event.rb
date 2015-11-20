75727d30a075d6260cf55a6926a4f8363c259fc3
dschrag <dschrag@purdue.edu>
2015-11-20 12:55:06 -0500

    Removed files

--- roomedy/app/models/event.rb
+++
@@ -1,8 +0,0 @@
-class Event < ActiveRecord::Base
-	belongs_to :user
-	validates :user_id, presence: true
-	validates :name, presence: true
-	validates :description, presence: true
-	validates :start_time, presence: true
-	validates :end_time, presence: true
-end
